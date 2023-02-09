1400628bef1e44bff5af62361af5121f72eac405
Ernesto Zerda <57075140+quezerdada@users.noreply.github.com>
2023-02-08 23:32:38 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,19 @@ from boto3.dynamodb.conditions import Key
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
-# Hola amigos de Uniandes, los quiero mucho
+# Clase 14 a ver si lo logramos!!!
 
 class DynamoAccessor:
     def __init__(self, dynamo_table):
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, dni):
-        response = self.table.query(KeyConditionExpression=Key('dni').eq(dni))
+    def get_data_from_dynamo(self, cc):
+        response = self.table.query(KeyConditionExpression=Key('cc').eq(cc))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['dni'])
+    db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
     return db_element
 
